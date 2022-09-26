dd40a130c43ccf957fd3ef66fe33e5a6ec77a287
umarelllo <sloppyfinn1603@gmail.com>
2022-09-26 21:17:26 +0300

    lesson-2

---
+++ lesson2/script.js
@@ -0,0 +1,89 @@
+// let a = 5
+// let b = 10
+// 
+// function sum(a, b) {
+//     const c = a + b
+//     console.log(c)
+// }
+// sum(a, b)
+
+
+// function sum() {}
+// const sum2 = function () {}
+// () => {} - стрелочная функция
+
+
+// function d () {
+//     return 'name'
+// }
+// const s = {
+//     [d()]: 'Ogogo',
+// }
+// console.log(s)
+
+
+// function myFn(a, b) {
+//     let c
+//     a = a + b
+//     c = a + b
+//     return c
+// }
+// console.log(myFn(3, 4))
+// console.dir(myFn)
+
+
+// const personOne = {
+//     name: 'Sam',
+//     age: 24,
+// }
+// function incrementPersonAge(person) {
+//     const updatePerson = Object.assign({}, person)
+//     updatePerson.age = 1
+//     return updatePerson
+// }
+// const newPersoneOne = incrementPersonAge(personOne))
+// console.log (newPersoneOne)
+
+
+// function anotherFunction (a, b) {
+//     console.log(a + b)
+// }
+// function fnWithCallback (foo) {
+//     foo(5, 10)
+// }
+// fnWithCallback(anotherFunction)
+
+
+// function printNameUser() {
+//    console.log('Hi');
+// }
+// setTimeout(printNameUser, 1000)
+
+
+// if () {
+// 
+// } else {}
+// 
+// switch (key) {
+//     case 'string':
+// }
+// 5 < 2 ? console.log(1) : console.log(2);
+// if (5 < 10) {
+//     console.log('Вариант #1')
+//     return
+// } else if (5 == 10) {
+//     console.log('Вариант #2')
+// } else {
+//     console.log('Вариант else')
+// }
+// if (user) return console.log('Hello, User')
+// user ? console.log('Yes') : console.log('No')
+
+
+// let a = 5
+// switch (a) {
+//    case a > 3:
+//        console.log('a > 3')
+//    case a < 7:
+//        console.log('a < 7')
+// }
