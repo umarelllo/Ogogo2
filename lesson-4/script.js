2b4c0cffa0058d0ab0e9e978955f9265a8d75cf9
umarelllo <sloppyfinn1603@gmail.com>
2022-10-03 18:42:50 +0300

    lesson-5

---
+++ lesson-4/script.js
@@ -0,0 +1,12 @@
+// let blockHTML = document.querySelector('.block')
+// let htmlP = document.createElement('p')
+// let inp = document.querySelector('.inp')
+// htmlP.innerText = "Hi"
+// htmlP.style.color = "white"
+// htmlP.classList = "i"
+// console.log(inp)
+
+let htmlH = document.createElement('h1')
+htmlH.innerText = 'ВХОД'
+htmlH.style.color = 'black'
+document.append(htmlH)
